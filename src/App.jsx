3e4dd289aeec774bd7f7765de3b4b8ccbc5eed39
dgonc <d.goncalves89000@gmail.com>
2024-06-18 13:43:45 +0200

    correction commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-
-
-
-import './App.css'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import SectionArticles from './components/SectionArticles';
+import "./App.css";
 
 
 
 function App() {
 
+
   return (
-    <>
-    
-     
-    </>
+    <div>
+    <Navbar />
+    <Header />
+    <SectionArticles />
+    <Footer />
+  </div>
   )
 }
-
 export default App
